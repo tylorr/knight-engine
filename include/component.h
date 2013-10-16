fb68fb9194dbdb530c06edc03682aab7b5470bf1
miningold <rynldsrap@gmail.com>
2013-10-16 12:23:15 -0400

    cleanup component class

--- include/component.h
+++ include/component.h
@@ -1,38 +1,41 @@
 #ifndef COMPONENT_H_
 #define COMPONENT_H_
 
-#include "common.h"
+#include <memory>
 
 namespace knight {
 
 class Component {
  public:
-  Component(int type) : type_(type) { }
 
+  // Create pointer to component passing it a component Type
   template<typename T>
-  static T *Create() {
-    return new T(Component::TypeFor<T>());
+  static std::shared_ptr<T> Create() {
+    return std::shared_ptr<T>(new T(Component::TypeFor<T>()));
   }
 
-  int type() const { return type_; }
-
-  ID id() const { return id_; }
-  void set_id(ID id) { id_ = id; }
-
- private:
-  static int NextType() {
-    static int type(0);
-    return type++;
-  }
-
+  // Generate and remember type for class T
   template<typename T>
   static int TypeFor() {
     static int type(Component::NextType());
     return type;
   }
 
+  int type() const { return type_; }
+
+ protected:
+  explicit Component(int type) : type_(type) { }
+
+ private:
+  Component();
+
+  // Return an incremented type for each call
+  static int NextType() {
+    static int type(0);
+    return type++;
+  }
+
   int type_;
-  ID id_;
 };
 
 }; // namespace knight

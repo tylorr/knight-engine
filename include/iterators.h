9e81a5e2c4dba403ced0685af6d868ba052d3936
tylorr <tylor@fastmail.com>
2015-07-01 23:30:15 -0400

    fix array iterators not using alignment

--- include/iterators.h
+++ include/iterators.h
@@ -3,10 +3,17 @@
 #include <hash.h>
 
 namespace foundation {
-  template<typename T> T* begin(Array<T> &a) { return array::begin(a); }
-  template<typename T> const T* begin(const Array<T> &a) { return array::begin(a); }
-  template<typename T> T* end(Array<T> &a) { return array::end(a); }
-  template<typename T> const T* end(const Array<T> &a) { return array::end(a); }
+  template<typename T, uint32_t align>
+  T* begin(Array<T, align> &a) { return array::begin(a); }
+
+  template<typename T, uint32_t align>
+  const T* begin(const Array<T, align> &a) { return array::begin(a); }
+
+  template<typename T, uint32_t align>
+  T* end(Array<T, align> &a) { return array::end(a); }
+
+  template<typename T, uint32_t align>
+  const T* end(const Array<T, align> &a) { return array::end(a); }
 
   template<typename T> const typename Hash<T>::Entry *begin(const Hash<T> &h) { return hash::begin(h); }
   template<typename T> const typename Hash<T>::Entry *end(const Hash<T> &h) { return hash::end(h); }

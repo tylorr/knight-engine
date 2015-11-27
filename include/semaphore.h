c68716dabf08260efbc2e211ec78cf7de0345b97
tylorr <tylor@fastmail.com>
2015-11-26 21:39:43 -0800

    add max value option for semaphores

--- include/semaphore.h
+++ include/semaphore.h
@@ -2,29 +2,33 @@
 
 #include <mutex>
 #include <condition_variable>
+#include <limits>
+#include <algorithm>
 
 namespace knight {
 
 class Semaphore {
  public:
-  Semaphore(int count = 0) : count_{count} { }
+  Semaphore(int count = 0, int max = std::numeric_limits<int>::max())
+    : count_{count},
+      max_{max} { }
 
   void Notify() {
     {
-      std::lock_guard<std::mutex> lock(mutex_);
-      count_++;
+      std::lock_guard<std::mutex> lock{mutex_};
+      count_ = std::min(count_ + 1, max_);
     }
     condition_.notify_one();
   }
 
   void Wait() {
-    std::unique_lock<std::mutex> lock(mutex_);
+    std::unique_lock<std::mutex> lock{mutex_};
     condition_.wait(lock, [this]{ return count_ > 0; });
     count_--;
   }
 
   bool TryWait() {
-    std::lock_guard<std::mutex> lock(mutex_);
+    std::lock_guard<std::mutex> lock{mutex_};
     if (count_ > 0) {
       count_--;
       return true;
@@ -37,6 +41,7 @@ class Semaphore {
   std::mutex mutex_;
   std::condition_variable condition_;
   int count_;
+  const int max_;
 };
 
 } // namespace knight

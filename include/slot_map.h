40624f0834c297d5b0f2663955a8b7a3446ab1dc
tylorr <tylor@fastmail.com>
2016-01-28 09:42:36 -0800

    replace array with vector in SlotMap

--- include/slot_map.h
+++ include/slot_map.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #include "common.h"
+#include "vector.h"
 
 #include <memory.h>
-#include <array.h>
 
 #include <cstddef>
 
@@ -15,58 +15,52 @@ class SlotMap {
   const size_t kChunkSize = 256;
 
   SlotMap(foundation::Allocator &allocator)
-    : slot_table_(allocator),
-      free_list_(allocator) { }
+    : slot_table_{allocator},
+      free_list_{allocator} { }
 
-  SlotMap(SlotMap &&other)
-    : slot_table_(std::move(other.slot_table_)),
-      free_list_(std::move(other.free_list_)) { }
+  SlotMap(const SlotMap &) = delete;
+  SlotMap(SlotMap &&) = default;
+
+  SlotMap &operator=(const SlotMap &) = delete;
+  SlotMap &operator=(SlotMap &&) = default;
 
   ID Create();
   T *Get(const ID &id) const;
   void Destroy(ID id);
 
-  SlotMap &operator=(SlotMap &&other) {
-    slot_table_ = std::move(other.slot_table_);
-    free_list_ = std::move(other.free_list_);
-    return *this;
-  }
-
  private:
   typedef std::unique_ptr<T[]> Chunk;
 
-  foundation::Array<Chunk> slot_table_;
-  foundation::Array<typename ID::type> free_list_;
-
-  KNIGHT_DISALLOW_COPY_AND_ASSIGN(SlotMap);
+  Vector<Chunk> slot_table_;
+  Vector<typename ID::type> free_list_;
 };
 
 template<typename T, typename ID>
 ID SlotMap<T, ID>::Create() {
   // Are there no spare entities?
-  if (foundation::array::empty(free_list_)) {
-    auto slot_table_size = foundation::array::size(slot_table_);
-    foundation::array::reserve(free_list_, kChunkSize * (slot_table_size + 1));
+  if (free_list_.empty()) {
+    auto slot_table_size = slot_table_.size();
+    free_list_.reserve(kChunkSize * (slot_table_size + 1));
 
     // Mark entire chunk as free
     typename ID::type last_index = slot_table_size * kChunkSize;
     for (int i = kChunkSize - 1; i >= 0; --i) {
-      foundation::array::push_back(free_list_, last_index + i);
+      free_list_.push_back(last_index + i);
     }
 
     // Add new chunk to table
-    foundation::array::emplace_back(slot_table_, new T[kChunkSize]);
+    slot_table_.emplace_back(new T[kChunkSize]);
 
     // Reserve ID 0.0 (index.version) as 'null' ID
-    if (foundation::array::size(slot_table_) == 1) {
+    if (slot_table_.size() == 1) {
       T &first_object = slot_table_[0][0];
       first_object.id.version = 1;
     }
   }
 
   // get first free index
-  typename ID::type free_index = foundation::array::back(free_list_);
-  foundation::array::pop_back(free_list_);
+  typename ID::type free_index = free_list_.back();
+  free_list_.pop_back();
 
   // get object at index and update it's index
   T *object = &slot_table_[free_index / kChunkSize][free_index % kChunkSize];
@@ -80,7 +74,7 @@ T *SlotMap<T, ID>::Get(const ID &id) const {
   typename ID::type chunkIndex = id.index / kChunkSize;
 
   // Does the chunk exist?
-  if (chunkIndex < foundation::array::size(slot_table_)) {
+  if (chunkIndex < slot_table_.size()) {
     T *object = &slot_table_[chunkIndex][id.index % kChunkSize];
 
     // If the ids (specifically the versions) match return object else NULL
@@ -101,7 +95,7 @@ void SlotMap<T, ID>::Destroy(ID id) {
   // Increment version to generate unique id and invalidate old id
   object->id.version++;
 
-  foundation::array::push_back(free_list_, object->id.index);
+  free_list_.push_back(object->id.index);
 }
 
 } // namespace knight
